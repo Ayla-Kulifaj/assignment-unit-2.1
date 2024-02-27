8a9ad930a54ac517ebb324dfe410db5c43b26388
Ayla Kulifaj <aylakulifaj@gmail.com>
2024-02-27 11:17:29 -0500

    Assigned Lucky Number

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -10,8 +10,8 @@ let fullName= firstName + ' ' + lastName;
 console.log(fullName)
 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-//     Console log the value of `luckyNumber`
-
+let luckyNumber=7;//     Console log the value of `luckyNumber`
+console.log(luckyNumber)
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
