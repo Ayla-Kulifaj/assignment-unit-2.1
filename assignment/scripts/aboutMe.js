eae327754dab9a11a31df1d8ad48214c5131515f
Ayla Kulifaj <aylakulifaj@gmail.com>
2024-02-27 11:29:08 -0500

    Assigned introduction, dynamic variables

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -7,17 +7,18 @@ let lastName = 'Kulifaj';
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
 let fullName= firstName + ' ' + lastName; 
-console.log(fullName)
+console.log(fullName);
 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 let luckyNumber=7;//     Console log the value of `luckyNumber`
-console.log(luckyNumber)
+console.log(luckyNumber);
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
+let introduction= `My name is ${fullName}, and I think ${luckyNumber} is a winner!`;
 //     'My name is (full name), and I think (lucky number) is a winner!'.
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
-
+console.log(introduction);
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 //     Console log the value of `adventurous`
 
