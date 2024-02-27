b4409b93b2b2b246f7c4a9ae348064d2144f55ab
Ayla Kulifaj <aylakulifaj@gmail.com>
2024-02-27 11:36:50 -0500

    Assigning Number of Pets

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -29,6 +29,8 @@ let food="Perogies";
 console.log(food);
 // 8 - Create a variable called `pets` and set it to the value of the number of pets you have
 //     Console log the value of `pets`
+let pets=0;
+console.log(pets);
 
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 
